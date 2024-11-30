6601fd77cfa295b1386a70ff4e27e30e142892e1
Shug3n <187776366+Shug3n@users.noreply.github.com>
2024-11-30 22:21:43 +0100

    added the first login page (customer pov), fixed the fonts and added the appropriate logos

---
+++ login-page/src/App.js
@@ -0,0 +1,15 @@
+
+import LoginCustomer from './Components/Forms/logincustomer.jsx'
+import './App.css';
+
+
+function App() {
+  return (
+    <div>
+      <LoginCustomer />
+      
+    </div>
+  );
+}
+
+export default App;
